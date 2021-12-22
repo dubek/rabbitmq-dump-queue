92ba5eabe98eafa3175aec4c5ba21e3de36a1f33
ReallyLiri <reallyliri@gmail.com>
2021-12-22 09:24:41 +0200

    Single file and no message limitations

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path"
 	"strings"
@@ -13,13 +12,16 @@ import (
 	"github.com/rabbitmq/amqp091-go"
 )
 
+const headersAndPropertiesSuffix = "-headers+properties"
+
 var (
 	uri         = flag.String("uri", "amqp://guest:guest@localhost:5672/", "AMQP URI")
 	insecureTLS = flag.Bool("insecure-tls", false, "Insecure TLS mode: don't check certificates")
 	queue       = flag.String("queue", "", "AMQP queue name")
 	ack         = flag.Bool("ack", false, "Acknowledge messages")
-	maxMessages = flag.Uint("max-messages", 1000, "Maximum number of messages to dump")
+	maxMessages = flag.Uint("max-messages", 1000, "Maximum number of messages to dump or 0 for unlimited")
 	outputDir   = flag.String("output-dir", ".", "Directory in which to save the dumped messages")
+	singleFile  = flag.Bool("single-file", false, "Dump all messages to a single unified file")
 	full        = flag.Bool("full", false, "Dump the message, its properties and headers")
 	verbose     = flag.Bool("verbose", false, "Print progress")
 )
@@ -31,7 +33,10 @@ func main() {
 		flag.Usage()
 		os.Exit(2)
 	}
-	err := dumpMessagesFromQueue(*uri, *queue, *maxMessages, *outputDir)
+	err := os.MkdirAll(*outputDir, 0644)
+	if err == nil {
+		err = dumpMessagesFromQueue(*uri, *queue, *maxMessages, *outputDir, *singleFile)
+	}
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
@@ -50,7 +55,7 @@ func dial(amqpURI string) (*amqp091.Connection, error) {
 	return conn, err
 }
 
-func dumpMessagesFromQueue(amqpURI string, queueName string, maxMessages uint, outputDir string) error {
+func dumpMessagesFromQueue(amqpURI string, queueName string, maxMessages uint, outputDir string, singleFile bool) error {
 	if queueName == "" {
 		return fmt.Errorf("Must supply queue name")
 	}
@@ -71,7 +76,15 @@ func dumpMessagesFromQueue(amqpURI string, queueName string, maxMessages uint, o
 	}
 
 	verboseLog(fmt.Sprintf("Pulling messages from queue %q", queueName))
-	for messagesReceived := uint(0); messagesReceived < maxMessages; messagesReceived++ {
+
+	if singleFile {
+		err = writeToSingleFiles(outputDir, false, "[\n")
+		if err != nil {
+			return err
+		}
+	}
+
+	for messagesReceived := uint(0); maxMessages == 0 || messagesReceived < maxMessages; messagesReceived++ {
 		msg, ok, err := channel.Get(queueName,
 			*ack, // autoAck
 		)
@@ -84,31 +97,81 @@ func dumpMessagesFromQueue(amqpURI string, queueName string, maxMessages uint, o
 			break
 		}
 
-		err = saveMessageToFile(msg.Body, outputDir, messagesReceived)
+		if messagesReceived > 0 && singleFile {
+			err = writeToSingleFiles(outputDir, true, ",\n")
+			if err != nil {
+				return err
+			}
+		}
+
+		err = saveMessageToFile(msg.Body, outputDir, singleFile, messagesReceived)
 		if err != nil {
 			return fmt.Errorf("Save message: %s", err)
 		}
 
 		if *full {
-			err = savePropsAndHeadersToFile(msg, outputDir, messagesReceived)
+			err = savePropsAndHeadersToFile(msg, outputDir, singleFile, messagesReceived)
 			if err != nil {
 				return fmt.Errorf("Save props and headers: %s", err)
 			}
 		}
 	}
 
+	if singleFile {
+		err = writeToSingleFiles(outputDir, true, "\n]")
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
-func saveMessageToFile(body []byte, outputDir string, counter uint) error {
-	filePath := generateFilePath(outputDir, counter)
-	err := ioutil.WriteFile(filePath, body, 0644)
+func writeToSingleFiles(outputDir string, append bool, content string) error {
+	err := writeToFile(generateFilePath(outputDir, true, 0, ""), []byte(content), append)
+	if err != nil {
+		return fmt.Errorf("Failed to write file: %s", err)
+	}
+	if *full {
+		err = writeToFile(generateFilePath(outputDir, true, 0, headersAndPropertiesSuffix), []byte("[\n"), append)
+		if err != nil {
+			return fmt.Errorf("Failed to write file: %s", err)
+		}
+	}
+	return nil
+}
+
+func saveMessageToFile(body []byte, outputDir string, singleFile bool, counter uint) error {
+	filePath := generateFilePath(outputDir, singleFile, counter, "")
+	err := writeToFile(filePath, body, singleFile)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(filePath)
+	if !singleFile || counter == 0 {
+		fmt.Println(filePath)
+	}
 
+	return nil
+}
+
+func writeToFile(filePath string, body []byte, append bool) error {
+	openFlags := os.O_WRONLY | os.O_CREATE
+	if append {
+		openFlags |= os.O_APPEND
+	} else {
+		openFlags |= os.O_TRUNC
+	}
+	f, err := os.OpenFile(filePath, openFlags, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write(body)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -142,7 +205,7 @@ func getProperties(msg amqp091.Delivery) map[string]interface{} {
 	return props
 }
 
-func savePropsAndHeadersToFile(msg amqp091.Delivery, outputDir string, counter uint) error {
+func savePropsAndHeadersToFile(msg amqp091.Delivery, outputDir string, singleFile bool, counter uint) error {
 	extras := make(map[string]interface{})
 	extras["properties"] = getProperties(msg)
 	extras["headers"] = msg.Headers
@@ -152,19 +215,29 @@ func savePropsAndHeadersToFile(msg amqp091.Delivery, outputDir string, counter u
 		return err
 	}
 
-	filePath := generateFilePath(outputDir, counter) + "-headers+properties.json"
-	err = ioutil.WriteFile(filePath, data, 0644)
+	filePath := generateFilePath(outputDir, singleFile, counter, headersAndPropertiesSuffix)
+	err = writeToFile(filePath, data, singleFile)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(filePath)
+	if !singleFile || counter == 0 {
+		fmt.Println(filePath)
+	}
 
 	return nil
 }
 
-func generateFilePath(outputDir string, counter uint) string {
-	return path.Join(outputDir, fmt.Sprintf("msg-%04d", counter))
+func generateFilePath(outputDir string, singleFile bool, counter uint, suffix string) string {
+	fileName := "messages"
+	if !singleFile {
+		fileName = fmt.Sprintf("msg-%04d", counter)
+	}
+	if suffix != "" {
+		fileName += suffix
+	}
+	fileName += ".json"
+	return path.Join(outputDir, fileName)
 }
 
 func verboseLog(msg string) {
